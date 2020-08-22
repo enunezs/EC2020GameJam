cd10c954240ae51816e1a754c2f705df638b28da
Checkarn <69473759+Checkarn@users.noreply.github.com>
2020-08-22 15:11:03 +0200

    Number of elements into array
    2 rocks, 2 papers, 0 scissors = [1, 1, 2, 2]

--- MainGame.gd
+++ MainGame.gd
@@ -11,16 +11,18 @@ var main_deck = []
 var player_deck = []
 var cpu_deck = []
 
-enum Item {ROCK=1, PAPER=2, SCISSORS=3}
+enum {ROCK=1, PAPER=2, SCISSORS=3}
 
-enum Result {WIN=1, LOSE=2, DRAW=3}
+enum {WIN=1, LOSE=2, DRAW=3}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	randomize()
-	print(generate_deck(1,2,3))
-
+	print(generate_deck(0,0,0))
+	
+	print(generate_deck(15,5,0))
+	
 	pass # Replace with function body.
 
 # warning-ignore:unused_argument
@@ -34,8 +36,18 @@ func generate_random_deck(size):
 	
 # generates array with number of elements set as parameter
 # Example: (2 rocks, 3 papers, o scissors) results in [1 ,1, 2, 2, 2]
-func generate_deck(rock, paper, scissors):
-	pass
+func generate_deck(nrock, paper, scissors):
+	main_deck = []
+	for x in nrock:
+		main_deck.append(ROCK)
+	for x in paper:
+		main_deck.append(PAPER)
+	for x in scissors:
+		main_deck.append(SCISSORS)
+		
+		
+			
+	return main_deck
 
 func shuffle_deck():
 	
