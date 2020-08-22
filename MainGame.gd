ec0ad11d5feb02c276ad5d748a2914bf72d8fb0f
Ema <enunezsardinha@gmail.com>
2020-08-22 21:15:18 +0200

    finished main helper functions

--- MainGame.gd
+++ MainGame.gd
@@ -21,17 +21,12 @@ enum {WIN=1, LOSE=2, DRAW=3}
 func _ready():
 	
 	randomize()
-	var deck = generate_deck(4,2,2)
-	print(main_deck)
-	split_into_decks()
-	print(player_deck)
-	print(cpu_deck)
 
-	
-	
+	print(battle_cards(ROCK,ROCK))
+
 	pass # Replace with function body.
 
-# warning-ignore:unused_argument
+
 func generate_random_deck(size):
 	main_deck = []
 	for n in size:
@@ -50,27 +45,31 @@ func generate_deck(nrock, paper, scissors):
 	for x in scissors:
 		main_deck.append(SCISSORS)
 		
-		
-
+			
 	return main_deck
-	
-func shuffle_deck(deck):
-	return deck.shuffle()
 
+
+func shuffle_deck(deck):
+	deck.shuffle()
+
+
+# First half into player deck, second half into CPU. Clears base deck
 func split_into_decks():
 	player_deck = main_deck.slice(0,(len(main_deck)/2)-1)
 	cpu_deck = main_deck.slice(len(main_deck)/2, len(main_deck))
 	main_deck=[]
-	
-	
+		
 	pass
 
 #Returns WIN if player card wins, LOSE if cpu wins
 func battle_cards(player_card, cpu_card):
-	pass
-
-#returns number of cards 
-func count_cards(type):
+	if player_card == cpu_card:
+		return DRAW
+	if player_card-cpu_card ==1 or player_card-cpu_card ==-2:
+		return WIN 
 	
-	pass
+	return LOSE
+
+
+
 
