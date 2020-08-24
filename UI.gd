5e0c4d6ea8ff10f35cae301280f02906ba96e4a3
Ema <enunezsardinha@gmail.com>
2020-08-24 14:57:19 +0200

    Portraits with points

--- UI.gd
+++ UI.gd
@@ -2,6 +2,12 @@ extends CanvasLayer
 
 
 onready var n_score_text = $ScoreBox/HBoxContainer/Score
+
+
+onready var n_score_text_player = $KidSprite/ScoreFrame/Score
+onready var n_score_text_cpu = $GrannySprite/ScoreFrame/Score
+
+
 onready var n_rock_text = $CounterBox/VBoxContainer/RockContainer/RockCounter
 onready var n_paper_text = $CounterBox/VBoxContainer/PaperContainer/PaperCounter
 onready var n_scissors_text = $CounterBox/VBoxContainer/ScissorsContainer/ScissorsCounter
@@ -14,10 +20,14 @@ func _ready():
 
 func update_score(player_score, cpu_score):
 	n_score_text.text = str(player_score) + "-" + str(cpu_score)
-
+	n_score_text_player.text = str(player_score)
+	n_score_text_cpu.text = str(cpu_score)
 
 func clear_score():
 	n_score_text.text = ""
+	n_score_text_player.text = ""
+	n_score_text_cpu.text = ""
+
 
 
 #TODO ANIMATE going up!
